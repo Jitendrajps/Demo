16f8fdb368f822a2d809cf0896ae6185320cb703
Jitendra Prasad <37190493+Jitendrajps@users.noreply.github.com>
2019-09-19 16:52:50 +0530

    Code modified

---
+++ src/com/demo/multithreading/Message.java
@@ -0,0 +1,14 @@
+package com.demo.multithreading;
+
+public class Message {
+
+	private String message;
+
+	public Message(String message) {
+		this.message = message;
+	}
+	public String getMessage() {
+		return message;
+	}
+
+}
---
+++ src/com/demo/multithreading/ProducerConsumerTest.java
@@ -0,0 +1,22 @@
+package com.demo.multithreading;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class ProducerConsumerTest {
+
+	public static void main(String[] args) {
+		BlockingQueue<Message> queue = new ArrayBlockingQueue<>(10);
+		Producer producer = new Producer(queue);
+		Consumer consumer = new Consumer(queue);
+		
+		Thread producerThread = new Thread(producer);		
+		Thread consumerThread = new Thread(consumer);
+
+		producerThread.start();
+		consumerThread.start();
+		
+		
+	}
+
+}
